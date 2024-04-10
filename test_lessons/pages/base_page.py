55a364a0bd1a72de82d7657bf51964115fc2f3af
KVladimirr <korotin.vova@mail.ru>
2024-04-10 17:05:52 +0300

    login test moved to base page class

--- test_lessons/pages/base_page.py
+++ test_lessons/pages/base_page.py
@@ -2,6 +2,7 @@ from selenium.common.exceptions import NoSuchElementException, NoAlertPresentExc
 import math
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from test_lessons.pages.locators import BasePageLocators
 
 
 class BasePage:
@@ -50,3 +51,10 @@ class BasePage:
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
--- test_lessons/pages/locators.py
+++ test_lessons/pages/locators.py
@@ -24,3 +24,8 @@ class ProductPageLocators:
     CART_PRICE = (By.CSS_SELECTOR, '.alert.alert-safe.alert-noicon.alert-info.fade.in .alertinner p:first-child strong')
     MESSAGE_WITH_PRODUCT_IN_CART = (By.CSS_SELECTOR, '#messages div:first-child div strong')
     PRODUCT_NAME = (By.CSS_SELECTOR, '.col-sm-6.product_main h1')
+
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
